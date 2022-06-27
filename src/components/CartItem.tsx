c71d5d5275fb6c27bc985b936954abe7a3abfb04
Shay Gali <86621168+ShayGali@users.noreply.github.com>
2022-06-27 09:43:53 +0300

    Update CartItem.tsx

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -8,7 +8,7 @@ export function CartItem({ id, quantity }: CartItemProps) {
   const item = storeItems.find((item) => item.id === id);
   if (!item) return null;
   return (
-    <Stack direction="horizontal" gap={2} className="d-flex align=items-center">
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
       <img
         src={item.imgUrl}
         style={{ width: "125px", height: "75px", objectFit: "cover" }}
